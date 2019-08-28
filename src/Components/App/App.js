80f61528a5b4631597f6b523306dfe11665454de
Salvador Villalon <salvav1@uci.edu>
2019-08-28 08:45:08 -0700

    Summer 2019: Starting project by adding components

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { SearchBar } from "../../SearchBar/SearchBar.js";
+import { SearchResults } from "../../SearchResults/SearchResults.js";
+import { PlayList } from "../../Playlist/Playlist.js";
+
+import "./App.css";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <div className="App">
+                    <!-- Add a SearchBar component -->
+                    <div className="App-playlist">
+                        <!-- Add a SearchResults component -->
+                        <!-- Add a Playlist component -->
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
