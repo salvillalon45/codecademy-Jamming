ce68b5e20743e0bf1e1fcb2d1e432fc751a9b5d4
Salvador Villalon <salvav1@uci.edu>
2019-08-30 11:28:42 -0700

    fixed untracked files

--- src/util/tools.js
+++
@@ -1,8 +0,0 @@
-const Tools = {
-    redirectURIL : "http://localhost:3000/",
-    redirectURIP : "http://sv_jamming.surge.sh",
-    clientID : "335bda4551534b258297e3026fddbbd3",
-}
-
-export default Tools;
-
